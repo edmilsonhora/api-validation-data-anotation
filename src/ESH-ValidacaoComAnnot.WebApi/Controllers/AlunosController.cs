eff561c9ea1c010a3b2f62d79cf415cb864572a1
Edmilson Hora <edmilson_hora@yahoo.com.br>
2023-02-06 12:53:22 -0300

    Inicio

---
+++ src/ESH-ValidacaoComAnnot.WebApi/Controllers/AlunosController.cs
@@ -0,0 +1,30 @@
+﻿using ESH_ValidacaoComAnnot.WebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESH_ValidacaoComAnnot.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunosController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), StatusCodes.Status400BadRequest)]
+        public IActionResult Salvar(AlunoView view)
+        {
+            try
+            {
+                return Ok(view);
+            }            
+            catch (Exception ex)
+            {
+                return BadRequest(new { ex.Message });
+            }
+        }
+    }
+}
---
+++ src/ESH-ValidacaoComAnnot.WebApi/Models/AlunoView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESH_ValidacaoComAnnot.WebApi.Models
+{
+    public class AlunoView
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Campo Nome é obrigatório.")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Campo RA é obrigatório.")]
+        public string RA { get; set; }
+        [Range(1,99999,ErrorMessage ="Campo TurmaId é obrigatório.")]
+        public int TurmaId { get; set; }
+    }
+}
